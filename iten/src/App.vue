feaa544a08e470f05daddd6783970e2f2156e444
Flint Smith <flintsmith@gmail.com>
2025-04-12 18:58:24 -0500

    update travel itinerary details with new names and flight information

--- iten/src/App.vue
+++ iten/src/App.vue
@@ -2,7 +2,8 @@
 
 <template>
   <div class="itinerary">
-    <h1>John Smith Travel Itinerary</h1>
+    <h1>John Smith & Jessica Miller</h1>
+    <h1>Travel Itinerary</h1>
     <h2>telephone number: +1 210 385-9809 (Whatsapp available)</h2>
     <h2>alternate phone number: +1 415 860-2970</h2>
     <h3>email: flintsmith@gmail.com</h3>
@@ -10,13 +11,16 @@
 
     <div class="day centered">
       <p class="larger-text">
-        April 23, 2025 Blue Hotel 123 Main Street, Fes, Morocco phone number
-        123-234-345
+        April 21, 2025 
       </p>
+      <p>AUS to LHR BA0190 British Airways </p>
       <p class="larger-text">
-        April 25, 2025 Red Inn 456 Another Road, Marrakesh, Morocco phone number
-        444-222-0011
+        April 22, 2025 
       </p>
+      <p>SEN to RAK K8ZCQ51 easy jet  1440pm flight number 
+EZY5739	</p>
+      <p>Riad Palais Sebban 43 Derb My Abdellah Ben Hssein,, Rue el Ksour, Marrakech 40000, Morocco https://www.palaissebban.com/</p>
+      <p>April 25</p>
     </div>
   </div>
 </template>
